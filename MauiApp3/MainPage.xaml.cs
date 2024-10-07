82ec8e1935c89c74a7c956d0e77450fa32c6719e
jjohnsen123desktop <johnsenjeppe@gmail.com>
2024-10-07 13:40:07 +0200

    Opgaver lek11

---
+++ MauiApp3/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MauiApp3
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+
+
+            editor.Text += $"{button.Text}\n";
+        }
+    }
+
+}
---
+++ MauiApp4/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace MauiApp4
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuFlyoutItem_Clicked(object sender, EventArgs e)
+        {
+            MenuFlyoutItem text = (MenuFlyoutItem)sender;
+
+            lbl1.Text = text.Text;
+        }
+    }
+
+}
