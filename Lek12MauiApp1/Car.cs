1e5359f442c94f72ee026168a9b33d6375ece1a2
jjohnsen123desktop <johnsenjeppe@gmail.com>
2024-10-07 15:16:59 +0200

    opgaver lek 12

---
+++ Lek12MauiApp1/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lek12MauiApp1
+{
+    public class Car : INotifyPropertyChanged
+    {
+        public string Model { get; set; }
+        public int Horsepower { get; set; }
+        private bool isElectric;
+
+
+        public bool IsElectric
+        {
+            get
+            {
+                return isElectric;
+            }
+            set
+            {
+                if (isElectric != value)
+                {
+                    isElectric = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs(nameof(isElectric)));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged = delegate { };
+
+    }
+}
---
+++ Lek12MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace Lek12MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        Car car = new Car { Model = "Tesla Model Y", Horsepower = 342, IsElectric = true };
+
+        public MainPage()
+        {
+            InitializeComponent();
+            entrycontainer.BindingContext = car;
+        }
+
+        private void btn1_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Car", $"Model: {car.Model}, Horsepower: {car.Horsepower}, Is Electric: {car.IsElectric}", "Ok");
+        }
+    }
+}
